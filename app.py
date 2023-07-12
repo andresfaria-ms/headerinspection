6ce312501425170ed78a2819a7a8231628ffedf2
andresfaria-ms <111012282+andresfaria-ms@users.noreply.github.com>
2023-07-12 10:35:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+#import flask
+#app = flask.Flask(__name__)
+
+#@app.route("/")
+#def hello():
+#	out = ''
+#	for line in flask.request.headers:
+#		out += '<p>' + str(line) + '</p>'
+#	return(out)
 import flask
+
 app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
-	out = ''
-	for line in flask.request.headers:
-		out += '<p>' + str(line) + '</p>'
-	return(out)
+    out = '<h1>Request Information:</h1>'
+    out += '<p>URI Path: ' + flask.request.path + '</p>'
+    out += '<p>Query: ' + str(flask.request.args) + '</p>'
+    out += '<h1>Request Headers:</h1>'
+    for line in flask.request.headers:
+        out += '<p>' + str(line) + '</p>'
+    return out
